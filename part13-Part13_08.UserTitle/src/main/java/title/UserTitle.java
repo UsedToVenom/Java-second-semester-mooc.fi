4e82b7ba6e371f583f2763dcd1a5f2f4cea19bf6
Marek Szulc <marekj.szulc@gmail.com>
2025-07-09 22:15:18 +0200

    adding day 10 work

--- part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
+++ part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
@@ -1,7 +1,18 @@
 package title;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class UserTitle {
+
+public class UserTitle extends Application{
+
+    @Override
+    public void start(Stage window){
+        Parameters param = getParameters();
+        String title = param.getNamed().get("Title");
+        window.setTitle(title);
+        window.show();
+    }
 
 
 }
--- part13-Part13_09.MultipleViews/src/main/java/application/MultipleViews.java
+++ part13-Part13_09.MultipleViews/src/main/java/application/MultipleViews.java
@@ -1,9 +1,63 @@
 package application;
 
-public class MultipleViews {
+import javafx.scene.control.Button;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MultipleViews extends Application{
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(MultipleViews.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        BorderPane firstView = new BorderPane();
+        VBox secondView = new VBox();
+        GridPane thirdView = new GridPane();
+        
+        Scene first = new Scene(firstView);
+        Scene second = new Scene(secondView);
+        Scene third = new Scene(thirdView);
+        
+        
+        Button firstButton = new Button("To the second view!");
+        Label firstLabel = new Label("First view!");
+        firstView.setTop(firstLabel);
+        firstView.setCenter(firstButton);
+        
+        window.setScene(first);
+        window.show();
+        
+        firstButton.setOnMouseClicked((event) -> {
+            window.setScene(second);
+        });
+        
+        Label secondLabel = new Label("Second view!");
+        Button secondButton = new Button("To the third view!");
+        secondView.getChildren().addAll(secondButton, secondLabel);
+        
+        secondButton.setOnMouseClicked((event) -> {
+            window.setScene(third);
+        });
+        
+        Label thirdLabel = new Label("Third view!");
+        Button thirdButton = new Button("To the first view!");
+        thirdView.add(thirdLabel, 0, 0);
+        thirdView.add(thirdButton, 1, 1);
+        
+        thirdButton.setOnMouseClicked((event) -> {
+            window.setScene(first);
+        });
+                
+        
+        
+        
     }
 
 }
